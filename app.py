9a29be7228adb198c32c5a132492710a1dfedbec
julietaborgna <juliborgna8@gmail.com>
2026-03-07 12:11:06 +0100

    feat: add requests to check in futbin status

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
-print("Hola mundo")
+# print("Hola mundo")
+
+import requests
+
+#URL de un jugador de FutBin
+(ejemplo: Messi)
+url = "https://www.futbin.com/26/player/20801/lionel-messi"
+
+#Realizamos la peticion
+response = requests.get(url)
+
+#Imprimimos el código de estado
+print(f"Status Code: {response.status_code}")
+
+if response.status_code == 200:
+    print("¡Conexión exitosa con FutBin!")
+else:
+    print(f"Algo pasó. Código: {response.status_code}")
